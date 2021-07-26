b0d10533729f8b217123b72853e1f15a80394d91
AV-Felipe <aerogrado@gmail.com>
2021-07-26 10:55:21 -0300

    creating initial components

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
+import { UserIcon } from './Icons';
 
-export const Header = () => <div>header</div>;
+export const Header = () => (
+  <div>
+    <a href="./">Q & A</a>
+    <input type="text" placeholder="search..." />
+    <a href="./sigin">
+      <UserIcon />
+      <span>Sign In</span>
+    </a>
+  </div>
+);
